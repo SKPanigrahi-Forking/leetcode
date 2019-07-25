29495339b17a2b873f690a8e13531b1a4bfa697e
CC Li <turingfly@gmail.com>
2019-07-24 22:22:07 -0400

    DP

---
+++ LeetCode/src/dynamicProgramming/_085_MaximalRectangle.java
@@ -0,0 +1,58 @@
+/**
+ * Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its
+ * area.
+ *
+ * Example:
+ *
+ * Input: [ ["1","0","1","0","0"], ["1","0","1","1","1"], ["1","1","1","1","1"], ["1","0","0","1","0"] ]
+ *
+ * Output: 6
+ */
+
+class _085_MaximalRectangle {
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[] left = new int[cols];
+        int[] right = new int[cols];
+        int[] width = new int[cols];
+
+        Arrays.fill(right, cols);
+        int res = 0;
+        for (int i = 0; i < rows; i++) {
+            int curLeft = 0;
+            int curRight = cols;
+
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == '1') {
+                    width[j]++;
+                } else {
+                    width[j] = 0;
+                }
+            }
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == '1') {
+                    left[j] = Math.max(left[j], curLeft);
+                } else {
+                    left[j] = 0;
+                    curLeft = j + 1;
+                }
+            }
+            for (int j = cols - 1; j >= 0; j--) {
+                if (matrix[i][j] == '1') {
+                    right[j] = Math.min(right[j], curRight);
+                } else {
+                    right[j] = cols;
+                    curRight = j;
+                }
+            }
+            for (int j = 0; j < cols; j++) {
+                res = Math.max(res, (right[j] - left[j]) * width[j]);
+            }
+        }
+        return res;
+    }
+}
---
+++ LeetCode/src/dynamicProgramming/_1092_ShortestCommonSupersequence.java
@@ -0,0 +1,73 @@
+/**
+ * Given two strings str1 and str2, return the shortest string that has both str1 and str2 as subsequences.  If multiple
+ * answers exist, you may return any of them.
+ *
+ * (A string S is a subsequence of string T if deleting some number of characters from T (possibly 0, and the characters
+ * are chosen anywhere from T) results in the string S.)
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: str1 = "abac", str2 = "cab" Output: "cabac"
+ *
+ *
+ * Explanation:
+ *
+ * str1 = "abac" is a substring of "cabac" because we can delete the first "c".
+ *
+ * str2 = "cab" is a substring of "cabac" because we can delete the last "ac".
+ *
+ * The answer provided is the shortest such string that satisfies these properties.
+ *
+ *
+ * Note:
+ *
+ * 1 <= str1.length, str2.length <= 1000 str1 and str2 consist of lowercase English letters.
+ */
+
+class _1092_ShortestCommonSupersequence {
+    public String shortestCommonSupersequence(String str1, String str2) {
+        if (str1 == null || str2 == null || str1.equals("") || str2.equals("")) {
+            return "";
+        }
+        String res = "";
+        int len1 = str1.length();
+        int len2 = str2.length();
+        // build the Longest commen subsequence
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (arr1[i - 1] == arr2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+        int i = len1;
+        int j = len2;
+        while (i >= 0 && j >= 0) {
+            if (i == 0) {
+                res = str2.substring(0, j) + res;
+                break;
+            } else if (j == 0) {
+                res = str1.substring(0, i) + res;
+                break;
+            } else if (arr1[i - 1] == arr2[j - 1]) {
+                res = "" + arr1[i - 1] + res;
+                i--;
+                j--;
+            } else if (dp[i][j] == dp[i - 1][j]) {
+                res = "" + arr1[i - 1] + res;
+                i--;
+            } else if (dp[i][j] == dp[i][j - 1]) {
+                res = "" + arr2[j - 1] + res;
+                j--;
+            }
+        }
+        return res;
+    }
+}
