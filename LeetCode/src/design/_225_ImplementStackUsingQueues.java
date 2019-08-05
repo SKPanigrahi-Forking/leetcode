28f51e601d42fdc6b064e926e5990bc4ef001fda
CC Li <turingfly@gmail.com>
2019-08-04 22:33:22 -0400

    Design

--- LeetCode/src/design/_225_ImplementStackUsingQueues.java
+++ LeetCode/src/design/_225_ImplementStackUsingQueues.java
@@ -3,22 +3,21 @@ package design;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class _225_ImplementStackUsingQueues {
+class _225_ImplementStackUsingQueues {
+
 	private Queue<Integer> queue = new LinkedList<>();
 
 	// Push element x onto stack.
 	public void push(int x) {
 		queue.add(x);
 		for (int i = 1; i < queue.size(); i++) {
-			queue.add(queue.remove());
+			queue.add(queue.poll());
 		}
 	}
 
 	// Removes the element on top of the stack.
 	public int pop() {
-		int res = top();
-		queue.remove();
-		return res;
+		return queue.poll();
 	}
 
 	// Get the top element.
